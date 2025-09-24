4311062832ab9fdc8a42dd969eb597838fbf7c57
balecodex <balecodex17@gmail.com>
2025-09-24 17:43:18 +0700

    Update start.py

--- start.py
+++ start.py
@@ -1,12 +1,13 @@
-# start.py
 from pyrogram import filters
 from pyrogram.types import Message
 
-def register(bot):
-    @bot.on_message(filters.command("start") & filters.private)
-    async def start_handler(client, message: Message):
-        await message.reply_text(
-            "Halo! Bot siap digunakan.\n\n"
-            "tes tes tes\n\n"
-            "BALERAROBY"
-        )
+def register(bot, handler_list):
+    h = bot.on_message(filters.command("start") & filters.private)(start_handler)
+    handler_list.append(h)
+
+async def start_handler(client, message: Message):
+    await message.reply_text(
+        "Halo! Bot siap digunakan.\n\n"
+        "tes tes tes\n"
+        "BALERAROBY"
+    )
